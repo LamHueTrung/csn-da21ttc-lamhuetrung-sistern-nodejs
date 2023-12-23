c52e2cbf11fe9d47e5a7fc15cc574109302f3262
LHT <93436510+LamHueTrung@users.noreply.github.com>
2023-12-23 17:21:23 +0700

    Update AdminRoutes.js

--- routes/AdminRoutes.js
+++ routes/AdminRoutes.js
@@ -2,16 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const DotThucTapController = require('../controllers/DotThucTapController');
-const CongViecController = require('../controllers/CongViecController');
 const ThongTinCongBoController = require('../controllers/ThongTInCongBoController');
 
 router.get('/danhsachdotthuctap', DotThucTapController.danhsachdotthuctap);
 router.get('/danhsachcongbo', DotThucTapController.danhsachcongbo);
-router.get('/danhsachcongviec', CongViecController.danhsachcongviec);
 router.get('/thongtincongbo', ThongTinCongBoController.danhsachcongviec);
 
 router.post('/themdotthuctap', DotThucTapController.Dangkythongtin);
 router.post('/themdanhsachcongbo', DotThucTapController.themdanhsachcongbo);
-router.post('/themcongviec', CongViecController.Dangkythongtin);
 
 module.exports = router;
